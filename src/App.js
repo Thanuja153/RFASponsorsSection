9fcbd208213df6ed4f7944c79505fb71d80988c2
Thanuja Pavani Satti <tsatti9475@sdsu.edu>
2023-11-17 09:28:15 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import image1 from "./sponsorslogos/lqff-600w.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function ContainerExample() {
+    return (
+        <div className="app-container">
+            <Container>
+                <h1 className="text-center">Thanks for Trusting Us</h1>
+                <Row className="justify-content-center">
+                    <Col className="col-6 col-md-3  m-4 text-center mx-auto">
+                        <img
+                            src={image1}
+                            alt="intuitive"
+                            className="img-fluid   max-height-img"></img>
+                    </Col>
+                    <Col className="col-6 col-md-3  m-4  text-center mx-auto">
+                        <img
+                            src={image1}
+                            alt="intuitive"
+                            className=" img-fluid   max-height-img"></img>
+                    </Col>
+                    <Col className="col-6 col-md-3 m-4  text-center mx-auto">
+                        <img
+                            src={image1}
+                            alt="intuitive"
+                            className="img-fluid   max-height-img"></img>
+                    </Col>
+                    <Col className="col-6 col-md-3 m-4  text-center mx-auto">
+                        <img
+                            src={image1}
+                            alt="intuitive"
+                            className="img-fluid   max-height-img"></img>
+                    </Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col className="col-6 col-md-3 m-4  text-center mx-auto">
+                        <img
+                            src={image1}
+                            alt="intuitive"
+                            className=" img-fluid  max-height-img"></img>
+                    </Col>
+                    <Col className="col-6 col-md-3 m-4  text-center mx-auto">
+                        <img
+                            src={image1}
+                            alt="intuitive"
+                            className="img-fluid   max-height-img"></img>
+                    </Col>
+                    <Col className="col-6 col-md-3 m-4  text-center mx-auto">
+                        <img
+                            src={image1}
+                            alt="intuitive"
+                            className=" img-fluid  max-height-img"></img>
+                    </Col>
+                    <Col className="col-6 col-md-3 m-4 text-center mx-auto">
+                        <img
+                            src={image1}
+                            alt="intuitive"
+                            className=" img-fluid   max-height-img"></img>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
-export default App;
+export default ContainerExample;
